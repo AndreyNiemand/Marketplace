b409362a84428232b8996491c3ff6c1b9f46d873
Nikita <polkovnikovnikita99@yandex.ru>
2021-11-12 20:42:56 +0300

    template project

---
+++ MariElMarketplace/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MariElMarketplace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
