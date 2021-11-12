2e3547c466b06d3b0004c339c79eba04e8b19659
Nikita <polkovnikovnikita99@yandex.ru>
2021-11-12 21:22:47 +0300

    new scaffolded elements

---
+++ MariElMarketplace/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MariElMarketplace.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MariElMarketplace.Areas.Identity.IdentityHostingStartup))]
+namespace MariElMarketplace.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
